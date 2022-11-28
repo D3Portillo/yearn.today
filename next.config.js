0b8458c3b98e6e4bc56587e82e3a06a9425daf0b
D3Portillo <d3portillo@gmail.com>
2022-11-28 15:40:31 +0100

    enh(next.config.js): add raw.githubusercontent.com to next.images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type { import('next').NextConfig } */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["raw.githubusercontent.com"],
+    minimumCacheTTL: 6000000,
+  },
 }
 
 module.exports = nextConfig
