d937dad8ad217809f24e7b874554680df4c84b87
D3Portillo <d3portillo@gmail.com>
2022-11-29 03:55:12 +0100

    feat(VaultList.tsx): add example layout to list stable vaults

--- components/VaultList.tsx
+++ components/VaultList.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import type { Vault } from "@/pages/api/vaults"
+import { useEffect, useState } from "react"
 import { FaChevronRight } from "react-icons/fa"
+
+import ff from "@/lib/services/ff"
+import { formatCurreny } from "@/lib/currency"
 import CardContainer from "@/components/layout/CardContainer"
 
 function VaultList() {
+  const [list, setList] = useState<Vault[]>([])
+
+  useEffect(() => {
+    ff.get<Vault[]>(["/vaults"]).then(setList)
+  }, [])
+
   return (
-    <CardContainer className="mt-8">
+    <CardContainer className="mt-8 min-h-[32rem]">
       <h2>Stable Opportunities</h2>
       <p>Invest on USDC vaults to earn constant rewards.</p>
       <table className="w-full mt-8">
@@ -19,31 +30,45 @@ function VaultList() {
           </tr>
         </thead>
         <tbody>
-          <Vault />
-          <Vault />
-          <Vault />
-          <Vault />
-          <Vault />
+          {list.length === 0 && vaultLoadingItemsList}
+          {list.map((vault) => {
+            return <Vault key={`vault-${vault.symbol}`} {...vault} />
+          })}
         </tbody>
       </table>
     </CardContainer>
   )
 }
 
-function Vault() {
+const vaultLoadingItemsList = [...Array(5)].map((_, i) => (
+  <VaultLoadingState key={`empty-state-${i}`} />
+))
+
+function VaultLoadingState() {
+  return (
+    <tr className="text-left">
+      <th colSpan={2}>
+        <div className="bg-zinc-100 h-12 rounded-lg" />
+      </th>
+      <th className="p-2">
+        <div className="bg-zinc-100 h-12 rounded-lg animate-pulse" />
+      </th>
+      <th className="p-2">
+        <div className="bg-zinc-100 h-12 rounded-lg animate-pulse delay-75" />
+      </th>
+    </tr>
+  )
+}
+
+function Vault({ display_name, symbol, tvl, icon }: Vault) {
   return (
     <tr className="border-t border-zinc-100">
       <td className="p-2 w-12">
-        <Image
-          alt=""
-          width={42}
-          height={42}
-          src="https://raw.githubusercontent.com/yearn/yearn-assets/master/icons/multichain-tokens/1/0xdf55670e27bE5cDE7228dD0A6849181891c9ebA1/logo-128.png"
-        />
+        <Image alt="" width={42} height={42} src={icon} />
       </td>
-      <td className="px-2 py-4">USDC/AAVE</td>
-      <td className="px-2 py-4">$3233K USD</td>
-      <td className="px-2 py-4">$32 USD</td>
+      <td className="px-2 py-4">{display_name}</td>
+      <td className="px-2 py-4">{formatCurreny(tvl.tvl)} USD</td>
+      <td className="px-2 py-4">$0.00 USD</td>
       <td>
         <div className="flex justify-end items-center">
           <button className="bg-black py-2 text-lg px-6 rounded-full text-white">
