72ee35449b90d78433bfe60876f689760054543f
D3Portillo <d3portillo@gmail.com>
2022-11-30 01:42:18 +0100

    chores(lib/numbers.ts): add #formatNumberCompact utility

--- lib/numbers.ts
+++ lib/numbers.ts
@@ -2,4 +2,11 @@ const Formatter = new Intl.NumberFormat("en-EN", {
   maximumFractionDigits: 2,
 })
 
+const CompactFormatter = new Intl.NumberFormat("en-US", {
+  notation: "compact",
+  maximumFractionDigits: 1,
+})
+
 export const formatNumber = (value: any) => Formatter.format(value)
+export const formatNumberCompact = (value: any) =>
+  CompactFormatter.format(value)
