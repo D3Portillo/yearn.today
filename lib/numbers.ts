ef326e7443b4bb76cbb92c460599ced8f56af3bb
D3Portillo <d3portillo@gmail.com>
2022-11-30 03:37:50 +0100

    chores(lib/): add #formatUSDC utilty

--- lib/numbers.ts
+++ lib/numbers.ts
@@ -10,3 +10,4 @@ const CompactFormatter = new Intl.NumberFormat("en-US", {
 export const formatNumber = (value: any) => Formatter.format(value)
 export const formatNumberCompact = (value: any) =>
   CompactFormatter.format(value)
+export const formatUSDC = (value: any) => value / 1e6
