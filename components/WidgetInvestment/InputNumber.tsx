1b75b794a4e3845cefa1eaada6b5718f6dbe5c77
D3Portillo <d3portillo@gmail.com>
2022-12-04 21:37:23 +0100

    chores: add re-usable component set for WidgetInvestment/

---
+++ components/WidgetInvestment/InputNumber.tsx
@@ -0,0 +1,52 @@
+import { type HTMLProps, useEffect, useState } from "react"
+import { withPreventDefault } from "@/lib/inputs"
+
+function InputNumber<InputValueType = string>({
+  onChange,
+  value: externalValue,
+  maxValue,
+  ...props
+}: Omit<HTMLProps<HTMLInputElement>, "onChange" | "value"> & {
+  onChange: (n: InputValueType) => void
+  maxValue: InputValueType
+  value: InputValueType
+}) {
+  const [value, setValue] = useState<InputValueType>()
+
+  function handleOnChange(value: InputValueType) {
+    if (isFinite(value as any)) {
+      setValue(value)
+      onChange(value)
+    }
+  }
+
+  useEffect(() => {
+    setValue(externalValue as any)
+  }, [externalValue])
+
+  function handleSetMax() {
+    handleOnChange(maxValue)
+  }
+
+  return (
+    <label className="text-xl bg-white flex items-center border rounded ring-yearn-blue focus-within:ring-2">
+      <input
+        {...props}
+        value={value as any}
+        className="bg-transparent flex-grow p-2 outline-none"
+        onChange={({ target }) => handleOnChange(target.value as any)}
+        placeholder="0.000"
+        name="amount"
+        required
+      />
+      <button
+        onClick={withPreventDefault(handleSetMax)}
+        className="h-full px-4 border-l text-sm text-zinc-600"
+      >
+        MAX
+      </button>
+    </label>
+  )
+}
+
+export default InputNumber
---
+++ components/WidgetInvestment/Table.tsx
@@ -0,0 +1,25 @@
+import type { PropsWithChildren } from "react"
+
+function Table({ children }: PropsWithChildren) {
+  return (
+    <table className="text-zinc-600 w-full">
+      <tbody>{children}</tbody>
+    </table>
+  )
+}
+
+export function Row({
+  children,
+  title,
+}: PropsWithChildren<{
+  title: string
+}>) {
+  return (
+    <tr>
+      <td className="font-bold">{title}:</td>
+      <td className="pl-4">{children}</td>
+    </tr>
+  )
+}
+
+export default Table
