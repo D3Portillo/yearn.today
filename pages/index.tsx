50f93298a718513ffcbb459c563515c3b585ed2f
D3Portillo <d3portillo@gmail.com>
2022-12-25 12:02:37 +0100

    FEAT: fetch for data while validating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { YDaemonVault } from "@/types/shared"
 import type { GetServerSideProps } from "next"
-import { getAllVaults } from "@/lib/services/vaults"
+import { useEffect, useState } from "react"
 
+import { getAllVaults } from "@/lib/services/vaults"
 import VaultList from "@/components/VaultList"
 import MainLayout from "@/components/layout/MainLayout"
 import Summary from "@/components/Summary"
 
-export default function Home({ vaults }: { vaults: YDaemonVault[] }) {
+export default function Home({ vaults: _vaults }: { vaults: YDaemonVault[] }) {
+  const [vaults, setVaults] = useState(_vaults)
+
+  useEffect(() => {
+    // If cached vaults failed for any unknown resason and empty
+    // Fetch for vaults while rendering the LoadingState
+    if (_vaults.length <= 0) getAllVaults().then(setVaults)
+  }, [])
+
   return (
     <MainLayout>
       <Summary />
